33ffda181f2f711bd7704dca238eac4b505e9054
Kavx <kavx@kavx.Kavx>
2022-01-24 08:48:11 +0100

    Consumiendo la API regional de centros educativos

--- app/Http/Controllers/API/CentroController.php
+++ app/Http/Controllers/API/CentroController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Centro;
 use Illuminate\Http\Request;
 use App\Http\Resources\CentroResource;
+use Illuminate\Support\Facades\Http;
 
 class CentroController extends Controller
 {
@@ -14,9 +15,10 @@ class CentroController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        return CentroResource::collection(Centro::paginate());
+    public function index() {
+        //return CentroResource::collection(Centro::paginate());
+	$response = Http::get('https://datosabiertos.regiondemurcia.es/catalogo/api/action//datastore_search?resource_id=52dd8435-46aa-495e-bd2b-703263e576e7&limit=5');
+        return response()->json(json_decode($response));
     }
 
     /**
@@ -28,7 +30,6 @@ class CentroController extends Controller
     public function store(Request $request)
     {
         $centro = json_decode($request->getContent(), true);
-
         $centro = Centro::create($centro);
 
         return new CentroResource($centro);
@@ -42,7 +43,9 @@ class CentroController extends Controller
      */
     public function show(Centro $centro)
     {
-        return new CentroResource($centro);
+        //return new CentroResource($centro);
+        $response = Http::get('https://datosabiertos.regiondemurcia.es/catalogo/api/action//datastore_search?resource_id=52dd8435-46aa-495e-bd2b-703263e576e7&limit=5');
+        return response()->json(json_decode($response));
     }
 
     /**
